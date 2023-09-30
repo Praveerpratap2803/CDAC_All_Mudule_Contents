e4de140b183cbd0595759817cae2049bd4868502
Praveerpratap2803 <praveerpratap2803@gmail.com>
2023-10-01 00:24:08 +0530

    Working on dbms_oracle_Assignments

--- DataBase_DBMS/Day1/Day1_Sql_developer.sql
+++ DataBase_DBMS/Day1/Day1_Sql_developer.sql
@@ -117,8 +117,115 @@ select * from emp where regexp_like(job,'.{5}');
 --14
 select * from emp where regexp_like(job,'^A.{4}');
 
+--Q2. Solve the following
+--1. Retrieve the details (Name, Salary and dept no) of the emp who are working in
+--department code 20, 30 and 40.
+--2. Display the total salary of all employees . Total salary will be calculated as
+--sal+comm+sal*0.10
+--
+--3. List the Name and job of the emp who have joined before 1 jan 1986 and whose
+--salary range is between 1200and 2500. Display the columns with user defined Column
+--headers.
+--4. List the empno, name, and department number of the emp works under manager
+--with id 7698
+--
+--5. List the name, job, and salary of the emp who are working in departments 10 and
+--30.
+--
+--6. Display name concatenated with dept code separated by comma and space. Name
+--the column as ‘Emp info’.
+--
+--7. Display the emp details who do not have manager.
+--
+--8. Write a query which will display name, department no and date of joining of all
+--employee who were joined January 1, 1981 and March 31, 1983. Sort it based on date of
+--joining (ascending).
+--
+--9. Display the employee details where the job contains word ‘AGE’ anywhere in the Job
+--
+--11. List the details of the employee , whose names start with ‘A’ and end with ‘S’ or
+--whose names contains N as the second or third character, and ending with either ‘N’ or ‘S’.
+--
+--12. List the names of the emp having ‘_’ character in their name.
+--
+--Group functions
+--1. Display the Highest, Lowest, Total & Average salary of all employee. Label the columns
+--Maximum, Minimum, Total and Average respectively for each Department. Also round the
+--result to the nearest whole number.
+--2. Display Department no and number of managers working in that department. Label the
+--column as ‘Total Number of Managers’ for each department.
+--3. Get the Department number, and sum of Salary of all non managers where the sum is
+--greater than 20000.
+
+select * from emp;
+
+--Q1
+--1
+select * from emp where sal>2000 and comm>200;
+--2
+select * from emp where job='CLERK' and sal>2000;
+--3
+select * from emp where sal in (1250,1100,2850);
+--4
+select * from emp where sal >1250 and sal < 2850;
+--5
+select * from emp where regexp_like(ename,'AS$');
+--6
+select * from emp where regexp_like(job,'^C.*K$');
+--7
+select * from emp where regexp_like(job,'..L.*M..');
+--8
+select * from emp where sal not in (1250,1100,2850);
+--9
+select * from emp where sal not between 1250 and 2850;
+--10
+select * from emp where regexp_like(job,'^C..E.*k$');
+--11
+select * from emp where comm is null;// is operator is used with null
+--12
+select * from emp where sal = null and regexp_like(ename,'^S');
+--13
+select * from emp where regexp_like(job,'.....');
+select * from emp where regexp_like(job,'.{5}');
+--14
+select * from emp where regexp_like(job,'^A.{4}');
+
 --Q2
 --1
+select ename,sal,deptno from emp where deptno in (10,20,30);
+--2
+select sal+nvl(comm,0)+sal*0.1 tot_sal from emp;
+--3
+select ename NAME,job U_JOB from emp where hiredate<'1-JAN-1986' AND SAL BETWEEN 1200 AND 2500;
+--4
+SELECT EMPNO,ENAME,DEPTNO FROM EMP WHERE MGR=7698;
+--5
+SELECT EMPNO,JOB,SAL FROM EMP WHERE DEPTNO IN (10,30);
+
+--6
+SELECT CONCAT(ENAME,DEPTNO) FROM EMP;
+--7
+SELECT * FROM EMP WHERE MGR IS NULL;
+--8
+SELECT ENAME,DEPTNO,HIREDATE FROM EMP WHERE HIREDATE='01-JAN-1981' AND HIREDATE='31-MAR-1983' ORDER BY HIREDATE;
+--9
+SELECT * FROM EMP WHERE REGEXP_LIKE(JOB,'AGE');
+--11
+SELECT * FROM EMP WHERE ENAME LIKE 'A%S' or ((ENAME LIKE '_N%' or ENAME LIKE '__N%') AND (ENAME LIKE '%N' or ENAME LIKE '%S'));
+SELECT * FROM EMP WHERE REGEXP_LIKE(ENAME,'^A.*S$|^..?N.*[NS]$');
+
+--12
+SELECT * FROM EMP WHERE REGEXP_LIKE(ENAME,'_');
+
+
+--Group functions
+--1
+SELECT DEPTNO,MAX(SAL) Maximum,MIN(SAL),SUM(SAL),AVG(SAL) FROM EMP GROUP BY DEPTNO ;
+--2
+SELECT DEPTNO,count(MGR) Total_Number_of_Managers FROM EMP GROUP BY DEPTNO;
+--3
+SELECT DEPTNO,SUM(SAL) FROM EMP WHERE JOB !='MANAGER' GROUP BY DEPTNO HAVING SUM(SAL)>20000;
+
 
 
 
--- DataBase_DBMS/Day2/Day2_Sql_developer.sql
+++ DataBase_DBMS/Day2/Day2_Sql_developer.sql
@@ -1,90 +1,97 @@
-Practise
+--Assignment Day 2
+
+--Practise
 
 
-Assignment 2
 
-Practise DQL queries
-1
+
+
+
+
+--Assignment 2
+
+--Practise DQL queries
+--1
 select * from emp where sal>1500 and job='MANAGER';
-2
+--2
 select * from emp where sal>1200 and sal<2000;
-3
+--3
 select * from emp where sal in (1600,800,1900);
-4
+--4
  select * from emp where ename like '%R_';
-5
+--5
 select * from emp where ename like 'A%N';
 
-Q2
+--Q2
 
-1
+--1
 select * from emp where sal>1250 and deptno=30;
-2
+--2
 select * from emp where sal>=1250 and sal<=3000;
-3
+--3
 select * from emp where sal>1250 and sal<3000;
-4
+--4
 select * from emp where sal in (3000,1250,2500);
-5
+--5
 select * from emp where ename='SMITH';
-6
+--6
 select * from emp where ename like 'S%';
 select * from emp where regexp_like(ename,'^S');
-7
+--7
 select * from emp where ename like '%S';
 select * from emp where regexp_like(ename,'S$');
-8
+--8
 select * from emp where ename like '.I%';
 select * from emp where regexp_like(ename,'^.T');
-9
+--9
 select * from emp where ename like 'A%L%N';
 select * from emp where regexp_like(ename,'^A.*L.*N$');
-10
+--10
 select * from emp where ename like 'A_B%P_';
 select * from emp where regexp_like(ename,'^A.B.*P.$');
-11
+--11
 select * from emp where ename like 'A%' or ename like 'S%' or ename like 'W%';
 select * from emp where regexp_like(ename,'^[ASW]');
 
-practise aggregate function
-12
+--practise aggregate function
+--12
 select job, max(sal),min(sal) from emp group by job;
-13
+--13
 select count(*) from emp where comm is null;
-14
+--14
 select sum(sal) from emp where deptno=10;
-15
+--15
 select deptno,job,max(sal),avg(sal) from emp group by deptno,job order by deptno;
-16
+--16
 select deptno,max(sal) from emp group by deptno having deptno>15 order by deptno;
-17
+--17
 select deptno,sum(sal) from emp group by deptno having sum(sal)>3000;
 
-18
+--18
 select deptno from emp group by deptno having count(*)>5;
-19
+--19
  select job,count(sal) from emp where sal>2000 group by job;
-20
+--20
 select lower(ename) from emp;
-21
+--21
 select rpad(ename,15,' '),rpad(job,15,' ') from emp;
-22
+--22
 select mgr,max(sal),min(sal),avg(sal) from emp group by mgr;
-23
+--23
 select sum(sal+nvl(comm,0)) total_salary,avg(sal+nvl(comm,0)) avg_salary from emp where sal>2000 and deptno in (10,20);
-24
+--24
 select * from emp where extract(year from hiredate)=1980 and extract(month from hiredate)=8 and sal>1500 and sal<2500;
-25
+--25
 select * from emp where extract(month from hiredate)=5 and extract(month from hiredate)=8 and extract(month from hiredate)=12;
-26
+--26
 select ename,to_char(hiredate,'dd/mm/yy') from emp where job='CLERK' and comm is not null;
-27
+--27
 select concat(substr(ename,3,5),substr(job,length(job)-1,length(job))),empno,ename,job from emp;
-28
+--28
 select to_char(nvl(comm,0),'$9,99,999') from emp where regexp_like(ename,'^A.*N$');
-29
+--29
 
-30
+--30
 
 
 
