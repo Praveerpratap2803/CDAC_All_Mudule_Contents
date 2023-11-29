049bf5f715d9ad4b4d6349aafaa868adf391da8d
Praveerpratap2803 <praveerpratap2803@gmail.com>
2023-11-29 23:03:54 +0530

    worked on java oops collections

---
+++ Java_CDAC/Day7/Day7_Self_1/src/code/Demo1.java
@@ -0,0 +1,11 @@
+package code;
+
+public class Demo1 {
+	public Demo1(){
+		System.out.println("Default constructor");
+	}
+	public void add(int a,int b) {
+		System.out.println("add "+a+b);
+	}
+	
+}
---
+++ Java_CDAC/Day7/Day7_Self_2/src/demo1/test.java
@@ -0,0 +1,11 @@
+package demo1;
+import code.Demo1;
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo1 d1 = new Demo1();
+		d1.add(10, 20);
+	}
+
+}
